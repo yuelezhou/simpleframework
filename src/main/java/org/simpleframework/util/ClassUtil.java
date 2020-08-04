3e3f30537211dd049ecc1b63c33271163b641b01
yuelezhou <31025969+yuelezhou@users.noreply.github.com>
2020-08-05 00:23:51 +0800

    5-2 8分钟 提交

---
+++ src/main/java/org/simpleframework/util/ClassUtil.java
@@ -0,0 +1,13 @@
+package org.simpleframework.util;
+
+import java.util.Set;
+
+public class ClassUtil {
+
+   public static Set<Class<?>> extractPackageClass(String packageName){
+       //1获取到类的加载器
+       //2通过类加载器获取到加载的资源
+       //3依据不同的资源类型,采用不同的方式获取资源的集合
+
+   }
+}
