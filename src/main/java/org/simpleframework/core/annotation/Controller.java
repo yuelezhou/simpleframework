d35df20db96edb70914a239a503043c9165d0132
Zhou Le <yuelezhou@163.com>
2020-08-18 13:29:33 +0800

    before remote merge

---
+++ src/main/java/org/simpleframework/core/annotation/Controller.java
@@ -0,0 +1,11 @@
+package org.simpleframework.core.annotation;
+
+/**
+ * @ Author     ：zhoule
+ * @ Date       ：Created in 09:32 2020-07-26
+ * @ Description：
+ * @ Modified By：
+ * @ Version:
+ */
+public @interface Controller {
+}
