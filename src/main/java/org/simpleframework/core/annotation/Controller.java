54094e430c4ff6a829d6486f89a17c19351e634b
Zhou Le <yuelezhou@163.com>
2020-08-18 13:30:26 +0800

    before remote merge

--- src/main/java/org/simpleframework/core/annotation/Controller.java
+++ src/main/java/org/simpleframework/core/annotation/Controller.java
@@ -1,5 +1,10 @@
 package org.simpleframework.core.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @ Author     ：zhoule
  * @ Date       ：Created in 09:32 2020-07-26
@@ -7,5 +12,8 @@ package org.simpleframework.core.annotation;
  * @ Modified By：
  * @ Version:
  */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Controller {
+    
 }
